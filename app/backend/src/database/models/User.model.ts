b8f9e60d5d4ef64914d89f04465bff331534ca41
Rafaelindo <RafafriedelDev@outlook.com>
2022-08-17 19:57:12 -0300

    feat: inserindo model de user

---
+++ app/backend/src/database/models/User.model.ts
@@ -0,0 +1,45 @@
+import { INTEGER, Model, STRING } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  id!: number;
+  usermame!: string;
+  role!: string;
+  email!: string;
+  password!: string;
+}
+
+User.init(
+  {
+    id: {
+      type: INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      allowNull: false,
+      type: STRING,
+    },
+    role: {
+      allowNull: false,
+      type: STRING,
+    },
+    email: {
+      allowNull: false,
+      type: STRING,
+      unique: true,
+    },
+    password: {
+      allowNull: false,
+      type: STRING,
+    },
+  },
+  {
+    timestamps: false,
+    sequelize: db,
+    modelName: 'teams',
+  },
+);
+
+export default User;
---
+++ app/backend/src/database/models/user.model.js
@@ -0,0 +1,34 @@
+const Users = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'User',
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      username: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      role: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      email: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        unique: true,
+      },
+      password: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+    },
+    { timestamps: false },
+  );
+  return User;
+};
+
+module.exports = Users;
