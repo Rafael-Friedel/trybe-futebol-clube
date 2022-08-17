8ee0306c5df5cde7ef5d9a8c90877b6cfa171ef9
Rafaelindo <RafafriedelDev@outlook.com>
2022-08-17 20:17:47 -0300

    feat: inserindo as associações

--- app/backend/src/database/models/Team.model.ts
+++ app/backend/src/database/models/Team.model.ts
@@ -1,5 +1,6 @@
 import { INTEGER, Model, STRING } from 'sequelize';
 import db from '.';
+import Match from './Match.model';
 
 class Team extends Model {
   id!: number;
@@ -26,5 +27,11 @@ Team.init(
     modelName: 'teams',
   },
 );
+
+Match.hasMany(Team, { foreignKey: 'homeTeam', as: 'teamHome' });
+Match.hasMany(Team, { foreignKey: 'awayTeam', as: 'teamAway' });
+
+Team.belongsTo(Match, { foreignKey: 'homeTeam', as: 'teamHome' });
+Team.belongsTo(Match, { foreignKey: 'awayTeam', as: 'teamAway' });
 
 export default Team;
