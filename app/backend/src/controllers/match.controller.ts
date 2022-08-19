14e426f6c11b7cb62d819fea34118cca1a62f87b
Rafaelindo <RafafriedelDev@outlook.com>
2022-08-19 17:02:52 -0300

    feat: inserindo função de query com inprogress

--- app/backend/src/controllers/match.controller.ts
+++ app/backend/src/controllers/match.controller.ts
@@ -3,6 +3,12 @@ import MatchService from '../services/match.service';
 
 class MatchController {
   static async getAll(req: Request, res: Response) {
+    const { inProgress } = req.query;
+    if (inProgress) {
+      const isProgress = inProgress === 'true';
+      const allMatches = await MatchService.findAllByProgress(isProgress);
+      return res.status(200).json(allMatches);
+    }
     const allMatches = await MatchService.getAll();
     res.status(200).json(allMatches);
   }
