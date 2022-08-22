18188114fc3b2eba8bda71068efd3af70832a186
Rafaelindo <RafafriedelDev@outlook.com>
2022-08-22 16:24:23 -0300

    feat: implementando testes da rota de login

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -1,52 +1,128 @@
 import * as chai from 'chai';
-import Sinon, * as sinon from 'sinon';
+import * as Sinon from 'sinon';
 // @ts-ignore
 import chaiHttp = require('chai-http');
 
 import { app } from '../app';
-import Example from '../database/models/ExampleModel';
 
+import * as bcrypt from 'bcryptjs';
 import { Response } from 'superagent';
+import User from '../database/models/User.model';
+import IUser from '../interfaces/IUser.interface';
+import AuthService from '../services/auth.service';
 import UserService from '../services/user.service';
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('/login', () => {
-  before(() => {
-    sinon.restore();
+const userMock: IUser = {
+  id: 1,
+  username: 'any-user',
+  email: 'any-email',
+  password: 'any-password',
+  role: 'any-role',
+};
+
+describe('users', () => {
+  beforeEach(() => Sinon.restore());
+  describe('/login', () => {
+    it('should return 200 and one token', async () => {
+      Sinon.stub(User, 'findOne').resolves();
+      Sinon.stub(UserService, 'login').resolves();
+      Sinon.stub(AuthService, 'createToken').resolves('any-token');
+      const response: Response = await chai.request(app).post('/login');
+      expect(response.status).to.equal(200);
+      expect(response.body).to.be.deep.equal({ token: 'any-token' });
+    });
+
+    it('should return one error: All fields must be filled and 400', async () => {
+      const response: Response = await chai
+        .request(app)
+        .post('/login')
+        .send({ email: userMock.email });
+      expect(response.body).to.be.deep.equal({
+        message: 'All fields must be filled',
+      });
+      expect(response.status).to.equal(400);
+    });
+
+    it('should return one error: All fields must be filled and 400', async () => {
+      const response: Response = await chai
+        .request(app)
+        .post('/login')
+        .send({ password: userMock.password });
+      expect(response.body).to.be.deep.equal({
+        message: 'All fields must be filled',
+      });
+      expect(response.status).to.equal(400);
+    });
+
+    it('should return one error: Incorrect email or password and 401', async () => {
+      Sinon.stub(User, 'findOne').resolves(undefined);
+      const response: Response = await chai
+        .request(app)
+        .post('/login')
+        .send(userMock);
+      expect(response.status).to.equal(401);
+      expect(response.body).to.be.deep.equal({
+        message: 'Incorrect email or password',
+      });
+    });
+
+    it('should return one error: Incorrect email or password and 401', async () => {
+      Sinon.stub(User, 'findOne').resolves(userMock as User);
+      Sinon.stub(bcrypt, 'compare').resolves(undefined);
+      const response: Response = await chai
+        .request(app)
+        .post('/login')
+        .send(userMock);
+      expect(response.status).to.equal(401);
+      expect(response.body).to.be.deep.equal({
+        message: 'Incorrect email or password',
+      });
+    });
   });
-  it('should return 200', async () => {
-    Sinon.stub(UserService, 'validateEmailAndPassword').resolves(undefined);
-  });
-  /**
-   * Exemplo do uso de stubs com tipos
-   */
 
-  // let chaiHttpResponse: Response;
+  describe('/validate', () => {
+    beforeEach(() => Sinon.restore());
+    it('should return 200 and one role', async () => {
+      Sinon.stub(AuthService, 'validateToken').resolves();
+      Sinon.stub(User, 'findOne').resolves(userMock as User);
+      const response: Response = await chai.request(app).get('/login/validate');
+      expect(response.status).to.equal(200);
+      expect(response.body).to.be.deep.equal({ role: 'any-role' });
+    });
 
-  // before(async () => {
-  //   sinon
-  //     .stub(Example, "findOne")
-  //     .resolves({
-  //       ...<Seu mock>
-  //     } as Example);
-  // });
+    it('should return one error: Token must be a valid token and 401', async () => {
+      const response: Response = await chai.request(app).get('/login/validate');
+      expect(response.status).to.equal(401);
+      expect(response.body).to.be.deep.equal({
+        message: 'Token must be a valid token',
+      });
+    });
 
-  // after(()=>{
-  //   (Example.findOne as sinon.SinonStub).restore();
-  // })
+    it('should return one error: Token must be a valid token and 401', async () => {
+      const response: Response = await chai
+        .request(app)
+        .get('/login/validate')
+        .send('any-token');
 
-  // it('...', async () => {
-  //   chaiHttpResponse = await chai
-  //      .request(app)
-  //      ...
+      expect(response.status).to.equal(401);
+      expect(response.body).to.be.deep.equal({
+        message: 'Token must be a valid token',
+      });
+    });
 
-  //   expect(...)
-  // });
+    it('should return one error: Token must be a valid token and 401', async () => {
+      Sinon.stub(AuthService, 'validateToken').resolves();
+      Sinon.stub(User, 'findOne').resolves(undefined);
+      const response: Response = await chai.request(app).get('/login/validate');
 
-  it('Seu sub-teste', () => {
-    expect(false).to.be.eq(true);
+      expect(response.status).to.equal(401);
+      expect(response.body).to.be.deep.equal({
+        message: 'Token must be a valid token',
+      });
+    });
   });
 });
