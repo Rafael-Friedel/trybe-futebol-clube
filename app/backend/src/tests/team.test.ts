fbcc34de9a18eb47499c6686f13a96ec7f658f8c
Rafaelindo <RafafriedelDev@outlook.com>
2022-08-22 16:52:20 -0300

    feat: implementando testes da rota teams

---
+++ app/backend/src/tests/team.test.ts
@@ -0,0 +1,50 @@
+import * as chai from 'chai';
+import * as Sinon from 'sinon';
+import { Response } from 'superagent';
+import { app } from '../app';
+import Team from '../database/models/Team.model';
+import ITeam from '../interfaces/ITeam.interface';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+const teamMock: ITeam = {
+  id: 1,
+  teamname: 'any-team',
+};
+
+describe.only('team', () => {
+  describe('/teams', () => {
+    beforeEach(() => Sinon.restore());
+    it('should return 200 and one list of teams', async () => {
+      Sinon.stub(Team, 'findAll').resolves([teamMock as Team]);
+      const response: Response = await chai.request(app).get('/teams');
+      expect(response.status).to.equal(200);
+      expect(response.body).to.be.deep.equal([teamMock]);
+    });
+    it('should return 500', async () => {
+      Sinon.stub(Team, 'findAll').rejects();
+      const response: Response = await chai.request(app).get('/teams');
+      expect(response.status).to.equal(500);
+    });
+  });
+
+  describe('/teams/:id', () => {
+    beforeEach(() => Sinon.restore());
+    it('should return 200', async () => {
+      Sinon.stub(Team, 'findByPk').resolves(teamMock as Team);
+      const response: Response = await chai.request(app).get('/teams/1');
+      expect(response.status).to.equal(200);
+      expect(response.body).to.be.deep.equal(teamMock);
+    });
+
+    it('should return 500', async () => {
+      Sinon.stub(Team, 'findByPk').rejects();
+      const response: Response = await chai.request(app).get('/teams/1');
+      expect(response.status).to.equal(500);
+    });
+  });
+});
