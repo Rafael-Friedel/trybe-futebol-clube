f74080fe0312b1085e4243f16d2a90bd65813dfa
Rafaelindo <RafafriedelDev@outlook.com>
2022-08-23 20:07:28 -0300

    fix: corrigindo bug de numeros negativos

--- app/backend/src/services/leaderboard.service.ts
+++ app/backend/src/services/leaderboard.service.ts
@@ -11,7 +11,7 @@ t.team_name as time,
   cast(SUM(m.home_team_goals < m.away_team_goals) as unsigned integer)  AS totalLosses,
   cast(SUM(m.home_team_goals) as unsigned integer) AS goalsFavor,
   cast(SUM(m.away_team_goals) as unsigned integer) AS goalsOwn,
-  cast(SUM(m.home_team_goals - m.away_team_goals) as unsigned integer)  AS goalsBalance,
+  (SUM(m.home_team_goals)) - (SUM(m.away_team_goals)) AS goalsBalance,
 FORMAT((((SUM(m.home_team_goals > m.away_team_goals) * 3) + 
 SUM(m.home_team_goals = m.away_team_goals))/(COUNT(m.home_team)*3)*100), 2) as efficiency
 FROM TRYBE_FUTEBOL_CLUBE.teams AS t
@@ -23,7 +23,7 @@ ORDER BY totalPoints DESC,
 totalVictories DESC, 
 goalsBalance DESC,
 goalsFavor DESC,
-goalsOwn DESC;`;
+goalsOwn ASC;`;
 
 const queryBestInAway = `SELECT
 t.team_name as time,
@@ -35,11 +35,11 @@ t.team_name as time,
   cast(SUM(m.home_team_goals > m.away_team_goals) as unsigned integer)  AS totalLosses,
   cast(SUM(m.home_team_goals) as unsigned integer)  AS goalsFavor,
   cast(SUM(m.away_team_goals) as unsigned integer) AS goalsOwn,
-cast(SUM(m.away_team_goals - m.home_team_goals )  as unsigned integer) AS goalsBalance,
+(SUM(m.away_team_goals)) - (SUM(m.home_team_goals)) AS goalsBalance,
 FORMAT((((SUM(m.home_team_goals < m.away_team_goals) * 3) + 
 SUM(m.home_team_goals = m.away_team_goals))/(COUNT(m.home_team)*3)*100), 2) as efficiency
 FROM TRYBE_FUTEBOL_CLUBE.teams AS t
-INNER JOIN TRYBE_FUTEBOL_CLUBE.matches AS m
+LEFT JOIN TRYBE_FUTEBOL_CLUBE.matches AS m
 ON m.away_team = t.id
 WHERE m.in_progress = false
 GROUP BY t.team_name
@@ -47,7 +47,7 @@ ORDER BY totalPoints DESC,
 totalVictories DESC, 
 goalsBalance DESC,
 goalsFavor DESC,
-goalsOwn DESC;`;
+goalsOwn ASC;`;
 
 class leaderboardService {
   static async bestshome() {
