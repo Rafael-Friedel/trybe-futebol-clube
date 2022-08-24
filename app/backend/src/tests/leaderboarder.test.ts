918b64643d7cbcc7a3685cc88e7e123d1dd0274b
Rafaelindo <RafafriedelDev@outlook.com>
2022-08-24 18:42:08 -0300

    feat: adicionando testes de leaderboard

--- app/backend/src/tests/leaderboarder.test.ts
+++ app/backend/src/tests/leaderboarder.test.ts
@@ -16,15 +16,28 @@ const { expect } = chai;
 
 const tableMock: ClassificationTable = {
   name: 'any-team',
-  totalPoints: 1,
-  totalGames: 1,
+  totalPoints: 3,
+  totalGames: 2,
   totalVictories: 1,
   totalDraws: 1,
-  totalLosses: 1,
-  goalsFavor: 1,
+  totalLosses: 0,
+  goalsFavor: 2,
   goalsOwn: 1,
-  goalsBalance: '1',
-  efficiency: '100.00',
+  goalsBalance: 1,
+  efficiency: '50.00',
+};
+
+const tableMockResult: ClassificationTable = {
+  name: 'any-team',
+  totalPoints: 6,
+  totalGames: 4,
+  totalVictories: 2,
+  totalDraws: 2,
+  totalLosses: 0,
+  goalsFavor: 4,
+  goalsOwn: 2,
+  goalsBalance: 2,
+  efficiency: '50.00',
 };
 
 describe('leaderboard', () => {
@@ -69,6 +82,25 @@ describe('leaderboard', () => {
       const response: Response = await chai
         .request(app)
         .get('/leaderboard/away');
+      expect(response.status).to.equal(500);
+    });
+  });
+
+  describe.only('leaderboard', () => {
+    beforeEach(() => Sinon.restore());
+    it('should return 200 and a classification table correct', async () => {
+      Sinon.stub(db, 'query').resolves([[tableMock], Op.any] as [
+        ClassificationTable[],
+        unknown
+      ]);
+      const response: Response = await chai.request(app).get('/leaderboard');
+      expect(response.status).to.equal(200);
+      expect(response.body).to.deep.equal([tableMockResult]);
+    });
+
+    it('should return 500', async () => {
+      Sinon.stub(db, 'query').rejects();
+      const response: Response = await chai.request(app).get('/leaderboard');
       expect(response.status).to.equal(500);
     });
   });
