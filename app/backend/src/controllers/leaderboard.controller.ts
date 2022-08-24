e94d6351cc44178982a4188c22a6409a896f9705
Rafaelindo <RafafriedelDev@outlook.com>
2022-08-24 18:28:41 -0300

    refactor: quebrando funções

--- app/backend/src/controllers/leaderboard.controller.ts
+++ app/backend/src/controllers/leaderboard.controller.ts
@@ -1,42 +1,44 @@
 import { Request, Response } from 'express';
-import ClassificationTable from '../interfaces/ClassificationTable.interface';
-import leaderboardService from '../services/leaderboard.service';
+import CT from '../interfaces/ClassificationTable.interface';
+import LeaderboardService from '../services/leaderboard.service';
 
-const tableAll = (home: ClassificationTable[], team: ClassificationTable) => {
-  const homeTeam = home.find((team2) => team.name === team2.name);
-  const table = {
-    name: homeTeam?.name,
-    totalPoints: team.totalPoints + (homeTeam?.totalPoints || 0),
-    totalGames: team.totalGames + (homeTeam?.totalGames || 0),
-    totalVictories: team.totalVictories + (homeTeam?.totalVictories || 0),
-    totalDraws: team.totalDraws + (homeTeam?.totalDraws || 0),
-    totalLosses: team.totalLosses + (homeTeam?.totalLosses || 0),
-    goalsFavor: team.goalsFavor + (homeTeam?.goalsFavor || 0),
-    goalsOwn: team.goalsOwn + (homeTeam?.goalsOwn || 0),
-    goalsBalance: +team.goalsBalance + +(homeTeam?.goalsBalance || 0),
-  };
-  return {
-    ...table,
-    efficiency: ((table.totalPoints / (table.totalGames * 3)) * 100).toFixed(2),
-  };
-};
-
-class leaderboardController {
+class LeaderboardController {
   static async bestshome(req: Request, res: Response) {
-    const all = await leaderboardService.bestshome();
+    const all = await LeaderboardService.bestshome();
     res.status(200).json(all);
   }
 
   static async bestsAway(req: Request, res: Response) {
-    const all = await leaderboardService.bestsAway();
+    const all = await LeaderboardService.bestsAway();
     res.status(200).json(all);
   }
 
+  private static tableAll = (home: CT[], team: CT) => {
+    const homeTeam = home.find((team2) => team.name === team2.name);
+    const table = {
+      name: homeTeam?.name,
+      totalPoints: team.totalPoints + (homeTeam?.totalPoints || 0),
+      totalGames: team.totalGames + (homeTeam?.totalGames || 0),
+      totalVictories: team.totalVictories + (homeTeam?.totalVictories || 0),
+      totalDraws: team.totalDraws + (homeTeam?.totalDraws || 0),
+      totalLosses: team.totalLosses + (homeTeam?.totalLosses || 0),
+      goalsFavor: team.goalsFavor + (homeTeam?.goalsFavor || 0),
+      goalsOwn: team.goalsOwn + (homeTeam?.goalsOwn || 0),
+      goalsBalance: +team.goalsBalance + +(homeTeam?.goalsBalance || 0),
+    };
+    return {
+      ...table,
+      efficiency: ((table.totalPoints / (table.totalGames * 3)) * 100).toFixed(
+        2,
+      ),
+    };
+  };
+
   static async bestTeams(req: Request, res: Response) {
-    const away = await leaderboardService.bestsAway();
-    const home = await leaderboardService.bestshome();
-    const test = away
-      .map((team) => tableAll(home, team))
+    const away = await LeaderboardService.bestsAway();
+    const home = await LeaderboardService.bestshome();
+    const allTeams = away
+      .map((team) => LeaderboardController.tableAll(home, team))
       .sort(
         (b, a) =>
           a.totalPoints - b.totalPoints
@@ -45,8 +47,8 @@ class leaderboardController {
           || a.goalsFavor - b.goalsFavor
           || b.goalsOwn - a.goalsOwn,
       );
-    res.status(200).json(test);
+    res.status(200).json(allTeams);
   }
 }
 
-export default leaderboardController;
+export default LeaderboardController;
